c76e0615797631ce13a2cd3c98fc371be2555fa4
Darlinghaven <Darlinghaven@outlook.com>
2024-11-25 00:50:56 +0100

    Afleveringsversionen

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,50 @@ namespace PersonligProfil
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenUrl("https://drive.google.com/file/d/1rp4VIyaLU7GiKf0tpeVV771bYZvbhObY/view?usp=drive_link");
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenUrl("https://github.com/darlinghaven");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            OpenUrl("https://instagram.com/darlinghaven");
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            OpenUrl("https://linkedin.com/in/darlinghaven");
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            OpenUrl("https://twitch.tv/darlinghaven");
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            OpenUrl("https://youtube.com/@darlinghavenDK");
+        }
+        private void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show($"Failed to open link: {ex.Message}");
+            }
         }
     }
 }
